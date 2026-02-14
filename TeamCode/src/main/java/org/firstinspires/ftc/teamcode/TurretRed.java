48e4b7bbc85cad2dbeddba09ca45adfc89cb5aed
JeriahNeal1 <jneal2@cocc.edu>
2026-02-13 20:14:51 -0800

    Pretty damn good tracking with the controller, all failsafes seem to work. Need to improve camera tracking, weird twitching condition when the wraparound function runs where the turret twitches back and forth around the 0 point..
    I think i need some kind of timeout around when the turret will switch back to controller controlled, Im guessing that is why the twitching occurs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretRed.java
@@ -22,17 +22,25 @@ public class TurretRed extends OpMode {
     Limelight3A limelight;
     public double targetAngle;
     public double turretAngle;
+    public double output;
+
     // --- Compute magnitude ---
     public double deadband = 0.2;
-    public static double kP = 0.01;
-    public static double MAX_ANGLE = 170;
-    public static double MIN_ANGLE = -170;
-    double magnitude;
+    public static double kP = 0.008;
+    public static double kF = 0.012;
+    public static double MAX_ANGLE = 175;
+    public static double MIN_ANGLE = -175;
+    public static double maxPower = 0.6;
+    public static double goaltarget = 0;
+    public double lastOutput = 0;
+    public static double maxChange = 0.017;
+    public double lastGoodtTx = 0;
+
     double ticksPerTurretRev = 537.7 * (200.0 / 86.0);
 
     @Override
     public void init(){
-    turretMotor = hardwareMap.get(DcMotorEx.class, "turret");
+        turretMotor = hardwareMap.get(DcMotorEx.class, "turret");
         turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         turretMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -40,7 +48,7 @@ public class TurretRed extends OpMode {
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.pipelineSwitch(1); // Switch to pipeline number 1
 
-}
+    }
 
     @Override
     public void start(){
@@ -49,34 +57,68 @@ public class TurretRed extends OpMode {
 
     @Override
     public void loop() {
+
         double x = gamepad2.right_stick_x;
         double y = -gamepad2.right_stick_y;
-        double stickAngle = Math.toDegrees(Math.atan2(x, y));
+        double magnitude = Math.hypot(x, y);
 
-// If stick is near center, don't update target
-        if (magnitude > 0.2) {
-            targetAngle = stickAngle;
+        turretAngle = (turretMotor.getCurrentPosition() / ticksPerTurretRev) * 360.0;
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            lastGoodtTx = result.getTx();
+            targetAngle = turretAngle + goaltarget - lastGoodtTx;
+        } else {
+            if (magnitude > 0.2) {
+                targetAngle = Math.toDegrees(Math.atan2(x, y));
+                if (targetAngle > MAX_ANGLE){
+                    targetAngle = MAX_ANGLE-5;
+                } else if (targetAngle < MIN_ANGLE){
+                    targetAngle = MIN_ANGLE+5;
+                }
+            }
+        }
+        // ---- WRAPPED ERROR ----
+        double error = normalizeAngle(targetAngle - turretAngle);
+
+        // ---- LIMIT AWARE PATH SELECTION ----
+        double proposedAngle = turretAngle + error;
+
+        if (proposedAngle > MAX_ANGLE || proposedAngle < MIN_ANGLE) {
+            if (error > 0) {
+                error -= 360;
+            } else {
+                error += 360;
+            }
         }
 
-        turretAngle = turretMotor.getCurrentPosition();
+        // ---- PID ----
+        output = kP * error;
 
-// Clamp target inside safe range
-        targetAngle = Math.max(MIN_ANGLE, Math.min(MAX_ANGLE, targetAngle));
+        if (Math.abs(error) > 1.0) {
+            output += Math.signum(error) * kF;
+        }
 
-// Simple linear error (NO normalize)
-        double error = targetAngle - turretAngle;
+// Clamp max torque
+        output = Math.max(-maxPower, Math.min(maxPower, output));
 
-// Soft stop protection
-        if (turretAngle >= MAX_ANGLE && error > 0) error = 0;
-        if (turretAngle <= MIN_ANGLE && error < 0) error = 0;
+// Slew rate limit
 
-        double output = kP * error;
+ double delta = output - lastOutput;
+
+        if (delta > maxChange) delta = maxChange;
+        if (delta < -maxChange) delta = -maxChange;
+
+        output = lastOutput + delta;
+        lastOutput = output;
 
         turretMotor.setPower(output);
 
-        telemetry.addData("magnitude", magnitude);
         telemetry.addData("turret angle", turretAngle);
         telemetry.addData("target angle", targetAngle);
-
+    }
+    public double normalizeAngle(double angle) {
+        while (angle > 180) angle -= 360;
+        while (angle < -180) angle += 360;
+        return angle;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/flywheelTest.java
@@ -3,18 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Flywheeltest", group = "Competition")
 
 public class flywheelTest extends OpMode {
-    public DcMotor intake, flywheel;
+    public DcMotor intake;
     @Override
     public void init(){
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
-
+        intake = hardwareMap.get(DcMotor.class, "turret");
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
         intake.setPower(1);
-        flywheel.setPower(0);
+
     }
 
     @Override
